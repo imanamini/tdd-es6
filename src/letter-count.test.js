8ab8b148afed75e2c1686afd5d1c3f982642631d
imanamini <iman.fa88@yahoo.com>
2021-05-15 12:33:18 +0430

    first commit

---
+++ src/letter-count.test.js
@@ -0,0 +1,21 @@
+import {expect} from "chai";
+import {getLetterCount} from './letter-count';
+
+describe('getLetterCount - basic functionality',()=>{
+    it('returns an empty object when passed an empty string ', ()=> {
+        const expected={};
+        const actual = getLetterCount('');
+        expect(actual).to.deep.equal(expected);
+    });
+    it('return the correct letter count', ()=> {
+        const expected = {c:1,a:1,t:1}
+        const actual = getLetterCount('cat')
+        expect(actual).to.deep.equal(expected)
+    });
+
+    it('return the correct count for words have more than certain word',  ()=> {
+        const expected = {m:1,i:4,s:4,p:2}
+        const actual = getLetterCount('mississippi')
+        expect(actual).to.deep.equal(expected)
+    });
+})
