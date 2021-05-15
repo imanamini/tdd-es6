cf61023407a6927803f6450bc5e01c127ad922ff
imanamini <iman.fa88@yahoo.com>
2021-05-15 14:58:38 +0430

    anagram final

--- src/anagrams.js
+++ src/anagrams.js
@@ -1,8 +1,9 @@
+import {isEqual} from 'lodash';
 import {getLetterCount} from "./letter-count";
 
 export const isAnagram = (string1,string2)=>{
-    const string1Letters = string1.split('')
-    const string2Letters = string2.split('')
+    const string1LettersCount = getLetterCount(string1)
+    const string2LettersCount = getLetterCount(string2)
 
-    return string1Letters.every(letter => string2Letters.includes(letter)) && string2Letters.every(letter => string1Letters.includes(letter)) && string1Letters.length === string2Letters.length
+    return isEqual(string1LettersCount,string2LettersCount)
 }
