ec673be05bead8a27c0e685afa849d12a4f04f32
imanamini <iman.fa88@yahoo.com>
2021-05-15 15:23:45 +0430

    multiple

---
+++ src/multiple.js
@@ -0,0 +1,4 @@
+export const multipleFunction = (num1,num2) =>{
+    const multiple = num1 * num2
+    return multiple
+}
---
+++ src/multiple.test.js
@@ -0,0 +1,10 @@
+import {expect} from "chai";
+import {multipleFunction} from "./multiple"
+
+describe('multiple numbers', ()=>{
+    it('should multiple two numbers',  ()=> {
+        const expected = 12
+        const actual = multipleFunction(3,4)
+        expect(actual).to.equal(expected)
+    });
+})
