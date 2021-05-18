212ea0875bb8d21890ebf41770d47737248df635
imanamini <iman.fa88@yahoo.com>
2021-05-18 17:47:43 +0430

    add Jest

--- src/multiple.test.js
+++ src/multiple.test.js
@@ -1,10 +1,11 @@
-import {expect} from "chai";
+// import {expect} from "chai";
 import {multipleFunction} from "./multiple"
 
 describe('multiple numbers', ()=>{
     it('should multiple two numbers',  ()=> {
-        const expected = 12
-        const actual = multipleFunction(3,4)
-        expect(actual).to.equal(expected)
+        // const expected = 12
+        expect(multipleFunction(3, 4)).toBe(12);
+        // const actual = multipleFunction(3,4)
+        // expect(actual).to.equal(expected)
     });
 })
